1cd9f8e4da58340fdff29a476ccd326d7c25b9a2
Cory Leach <cory.leach@gmail.com>
2019-11-20 18:25:47 -0500

    Fix warnings.

---
+++ Tests/Editor/ScriptableObjectsEditorTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Gameframe.ScriptableObjects.Tests.Editor
+{
+    public class ScriptableObjectsEditorTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void ScriptableObjectsEditorTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator ScriptableObjectsEditorTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
